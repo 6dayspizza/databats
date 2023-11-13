10f2fb525c2a5db64eaa826c14454904e34d0d7c
Ruth K. <dol.guldur@hotmail.com>
2023-11-13 17:04:52 +0100

    changes to gitignore

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,3 +1,4 @@
+
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
 
@@ -12,3 +13,23 @@ var pool = mysql.createPool({
 
 // Export it for use in our applicaiton
 module.exports.pool = pool;
+
+
+// this is for local use because I have a really
+// hard time connecting to the OSU servers
+
+/*
+// Get an instance of mysql we can use in the app
+var mysql = require('mysql');
+
+// Create a 'connection pool' using the default credentials
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',  // Default host for local installations
+    user: 'root',       // Default user for local installations
+    password: '',       // Default password is often empty for local installations
+    database: 'CS_340'    // Default test database
+});
+
+// Export it for use in our application
+module.exports.pool = pool; */
