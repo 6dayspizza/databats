3c52a1fd60e3ccbd6edab3437f11beeb926771cf
6dayspizza <dol.guldur@hotmail.com>
2023-11-30 05:55:00 -0800

    db-connector

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,4 +1,4 @@
-/*
+
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
 
@@ -28,7 +28,7 @@ module.exports.pool = pool;
 
 // // Export it for use in our applicaiton
 // module.exports.pool = pool; 
-*/
+/*
 // this is for local use because I have a really
 // hard time connecting to the OSU servers
 
@@ -45,4 +45,4 @@ var pool = mysql.createPool({
  });
 
 // Export it for use in our application
-module.exports.pool = pool;
+module.exports.pool = pool;*/
