cb398d228d73ed3dce1dc30b31cb08e315547a90
6dayspizza <55084543+6dayspizza@users.noreply.github.com>
2023-12-04 11:20:38 +0100

    fixed proper citation/comments

--- views/public/js/update_bat.js
+++ views/public/js/update_bat.js
@@ -1,27 +1,33 @@
-// Get the objects we need to modify
+// Citation for the following function:
+// Date: 10/12/2023
+// Partially based on: code from Dr. Curry
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+
+// GETS OBJECTS TO MODIFY
 let updateBatForm = document.getElementById('update-bat-form-ajax');
 
-// Modify the objects we need
+// MODIFIES THIS OBJECT
 updateBatForm.addEventListener("submit", function (e) {
 
-    // Prevent the form from submitting
+  // PREVENTS DEFAULT BEHAVIOUR ASSOCIATED WITH EVENT
+  // DO NOT REMOVE THIS LINE
     e.preventDefault();
 
-    // Get form fields we need to get data from
+  // ASSIGNS FIELDS FROM FORM WE JUST RECEIVED
     let inputIDBat = document.getElementById("idToUpdate");
     let inputEndDate = document.getElementById("input_enddate_update");
     let inputReleaseSite = document.getElementById("input_releasesite_update");
     let inputStatus= document.getElementById("input_status_update");
     let inputRemark= document.getElementById("input_remark_update");
 
-    // Get the values from the form fields
+  // EXTRACTS VALUES FROM ASSIGNED FIELDS
     let idBatValue = inputIDBat.value;
     let endDateValue = inputEndDate.value;
     let releaseSiteValue = inputReleaseSite.value;
     let statusValue = inputStatus.value;
     let remarkValue = inputRemark.value;
 
-    // Put our data we want to send in a javascript object
+  // PLACES THOSE VALUES IN JS OBJECT
     let bat = {
         idbat: idBatValue,
         enddate: endDateValue,
@@ -30,16 +36,16 @@ updateBatForm.addEventListener("submit", function (e) {
         remark: remarkValue,
     }
 
-    // Setup our AJAX request
+  // SETS UP AJAX REQUEST
     var xhttp = new XMLHttpRequest();
     xhttp.open("PUT", "/put-bat-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
-    // Tell our AJAX request how to resolve
+  // DEFINES BEHAVIOUR FOR AJAX
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
 
-            // refresh page
+           // RETURN TO PAGE
             window.location.href = '/bats';
 
         }
@@ -48,30 +54,31 @@ updateBatForm.addEventListener("submit", function (e) {
         }
     }
 
-    // Send the request and wait for the response
+  // SENDS REQUEST
     xhttp.send(JSON.stringify(bat));
 
 })
 
-
+// UPDATE METHOD
 function updateRow(bat, idBat) {
+    // GETS CURRENT OBJECT
     let parsedData = JSON.parse(bat);
 
+    // GETS CURRENT TABLE
     let table = document.getElementById("bats_table");
 
     for (let i = 0, row; row = table.rows[i]; i++) {
-        //iterate through rows
-        //rows would be accessed using the "row" variable assigned in the for loop
+        // ITERATES THROUGH ROWS
         if (table.rows[i].getAttribute("data-value") == idBat) {
 
-            // Get the location of the row where we found the matching person ID
+            // GET LOCATION OF ROW WITH MATCHING ID
             let updateRowIndex = table.getElementsByTagName("tr")[i];
 
-            // Get td of homeworld value
+            // GET TD VALUE
             let td = updateRowIndex.getElementsByTagName("td")[3];
 
-            // Reassign homeworld to our value we updated to
+            // REASSIGN VALUE
             td.innerHTML = parsedData[0].name;
         }
     }
-}
+};
