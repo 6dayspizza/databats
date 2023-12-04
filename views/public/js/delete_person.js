d83e1131030fad754ce7406ced249cc5474f8fac
6dayspizza <55084543+6dayspizza@users.noreply.github.com>
2023-12-04 11:10:18 +0100

    fixed proper citation/comments

--- views/public/js/delete_person.js
+++ views/public/js/delete_person.js
@@ -1,39 +1,39 @@
-   // Citation for the following function: 
-    //Date: 10/12/2023
-    //Copied from /OR/ Adapted from /OR/ Based on: code from Dr. Curry
-    //Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app 
+// Citation for the following function:
+// Date: 10/12/2023
+// Partially based on: code from Dr. Curry
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 
-// Delete methods
+// DELETE METHOD
 function deletePerson(idPerson) {
 
+   // ASK USER FOR CONFIRMATION
     var userConfirmed = window.confirm("are you sure you want to delete this person?");
 
     if (!userConfirmed) {
-        return; // Do nothing if the user cancels the deletion
+        return; // DO NOTHING IF CANCELLED
     }
 
-    // Put our data we want to send in a javascript object
+    // PLACES DATA IN JS OBJECT
     let person = {
         id: idPerson
     };
 
-    // Setup our AJAX request
+    // SETS UP AJAX REQUEST
     var xhttp = new XMLHttpRequest();
     xhttp.open("DELETE", "/delete-person-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
-    // Tell our AJAX request how to resolve
+    // DEFINES BEHAVIOUR FOR AJAX
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState == 4 && xhttp.status == 204) {
 
-            // deleteRow(idBat);
-
+           // RETURN TO PAGE
             window.location.href='/persons';
         }
         else if (xhttp.readyState == 4 && xhttp.status != 204) {
             console.log("There was an error with the input.")
         }
     }
-    // Send the request and wait for the response
+    // SENDS REQUEST
     xhttp.send(JSON.stringify(person));
-}
+};
